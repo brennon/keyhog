c5aad5aebe63a904c860c5b9b9295ccb29002fb1
Brennon Bortz <brennon@brennonbortz.com>
2013-04-18 13:39:28 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Keyhog::Application.config.secret_token = '4b675040cc07171e68de209779e4ac44002a3aa426f89efaac5de2300455e5cd86396ff890f3f86db311cd42453a8e647ec82f1ca52cb0b957a688cf5fc62b03'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Keyhog::Application.config.session_store :cookie_store, key: '_keyhog_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Keyhog::Application.config.session_store :active_record_store
