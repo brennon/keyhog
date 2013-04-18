b1e42c9dd90294a030a2efdc31191d52f756222c
Brennon Bortz <brennon@brennonbortz.com>
2013-04-18 14:07:05 -0400

    Add first validations to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :username
+
+  validates_uniqueness_of :username
+  validates_uniqueness_of :email
 end
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,7 +1,6 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  should validate_uniqueness_of :username
+  should validate_uniqueness_of :email
 end
