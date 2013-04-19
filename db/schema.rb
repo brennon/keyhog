efadca3d1ddeea9cc92fa1147e93c7010aeba2c0
Brennon Bortz <brennon@brennonbortz.com>
2013-04-18 22:41:05 -0400

    Add first and last names

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130418175153) do
+ActiveRecord::Schema.define(:version => 20130418184414) do
 
   create_table "users", :force => true do |t|
     t.string   "username"
     t.string   "email"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
 end
