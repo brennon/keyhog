b77ea5cc8f06eb86e9242306c6b416780a797483
Brennon Bortz <brennon@brennonbortz.com>
2013-04-20 11:49:47 -0400

    Switch to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -13,11 +13,6 @@
 
 ActiveRecord::Schema.define(:version => 20130420003848) do
 
-  create_table "pages", :force => true do |t|
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
   create_table "users", :force => true do |t|
     t.string   "username"
     t.string   "email"
