bad2c0f0139d90edabcc2769d93c3ca8483d6d93
Brennon Bortz <brennon@brennonbortz.com>
2013-04-27 16:05:26 -0400

    Test re-render edit form for bad new user

--- test/functional/users_controller_test.rb
+++ test/functional/users_controller_test.rb
@@ -30,6 +30,19 @@ class UsersControllerTest < ActionController::TestCase
     assert_redirected_to user_path(assigns(:user))
   end
 
+  test "should redisplay the new form if user does not save" do
+    post :create, user: {
+      username: nil,
+      email: @user.email,
+      first_name: @user.first_name,
+      last_name: @user.last_name,
+      password: @user.password,
+      password_confirmation: @user.password_confirmation
+    }
+    assert_template 'new'
+  end
+
+
   test "should show user" do
     @user.save
     session[:user_id] = @user.id
