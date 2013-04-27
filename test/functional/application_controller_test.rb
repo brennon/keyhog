703f8bfca9be0a10a9c641626120fc7d6ed1c191
Brennon Bortz <brennon@brennonbortz.com>
2013-04-27 16:22:17 -0400

    Test access denied redirect

--- test/functional/application_controller_test.rb
+++ test/functional/application_controller_test.rb
@@ -4,18 +4,33 @@ class ApplicationControllerTest < ActionController::TestCase
   setup do
   end
 
+  def perform_action_with(controller = nil, &block)
+    app_controller = @controller
+    @controller = controller.new
+    block.call
+    @controller = app_controller
+  end
+
   test ":current_user should return the logged-in user" do
     user = FactoryGirl.create(:user)
-    app_controller = @controller
-    @controller = SessionsController.new
-    post :create, { email: user.email, password: user.password }
-    @controller = app_controller
+    perform_action_with(SessionsController) do
+      post :create, { email: user.email, password: user.password }
+    end
     assert_equal user, @controller.send(:current_user)
   end
 
   test ":current_user should return nil if no user is logged in" do
     session[:user_id] = nil
     assert_nil @controller.send(:current_user)
+  end
+
+  test "should redirect to the login URL on access denied" do
+    user = FactoryGirl.create(:user)
+    session[:user_id] = nil
+    perform_action_with(UsersController) do
+      get :show, id: user.id
+    end
+    assert_redirected_to login_path
   end
 end
 
