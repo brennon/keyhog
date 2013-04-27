d32a0934035ed544d6074040d5a1e4d9f4fce8dc
Brennon Bortz <brennon@brennonbortz.com>
2013-04-27 16:34:43 -0400

    Remove extraneous JSON logic from certs controller

--- app/controllers/certificates_controller.rb
+++ app/controllers/certificates_controller.rb
@@ -8,36 +8,18 @@ class CertificatesController < ApplicationController
   end
 
   # GET /certificates
-  # GET /certificates.json
   def index
     @certificates = @user.certificates
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @certificates }
-    end
   end
 
   # GET /certificates/1
-  # GET /certificates/1.json
   def show
     @certificate = Certificate.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @certificate }
-    end
   end
 
   # GET /certificates/new
-  # GET /certificates/new.json
   def new
     @certificate = Certificate.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @certificate }
-    end
   end
 
   # GET /certificates/1/edit
@@ -46,46 +28,34 @@ class CertificatesController < ApplicationController
   end
 
   # POST /certificates
-  # POST /certificates.json
   def create
     @certificate = @user.certificates.build(params[:certificate])
 
-    respond_to do |format|
-      if @certificate.save
-        format.html { redirect_to [@user, @certificate], notice: 'Certificate was successfully created.' }
-        format.json { render json: @certificate, status: :created, location: @certificate }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @certificate.errors, status: :unprocessable_entity }
-      end
+    if @certificate.save
+      redirect_to [@user, @certificate], 
+        notice: 'Certificate was successfully created.'
+    else
+      render action: "new"
     end
   end
 
   # PUT /certificates/1
-  # PUT /certificates/1.json
   def update
     @certificate = Certificate.find(params[:id])
 
-    respond_to do |format|
-      if @certificate.update_attributes(params[:certificate])
-        format.html { redirect_to [@user, @certificate], notice: 'Certificate was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @certificate.errors, status: :unprocessable_entity }
-      end
+    if @certificate.update_attributes(params[:certificate])
+      redirect_to [@user, @certificate],
+        notice: 'Certificate was successfully updated.'
+    else
+      render action: "edit"
     end
   end
 
   # DELETE /certificates/1
-  # DELETE /certificates/1.json
   def destroy
     @certificate = Certificate.find(params[:id])
     @certificate.destroy
 
-    respond_to do |format|
-      format.html { redirect_to user_certificates_url(@user) }
-      format.json { head :no_content }
-    end
+    redirect_to user_certificates_url(@user)
   end
 end
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -34,9 +34,5 @@ class Ability
     can [:read, :update, :destroy], User, id: user.id
     can :create, User
     can :manage, Certificate, user_id: user.id
-
-    if user.email == 'brennon@vt.edu'
-      can :manage, :all
-    end
   end
 end
